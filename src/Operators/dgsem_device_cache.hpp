2fa303b34dacaf99d73d25b2d8a367abcb323801
Mike Campbell <mtcampbe@illinois.edu>
2026-02-18 10:02:12 -0600

    Add some data items supporting facial assembly

--- src/Operators/dgsem_device_cache.hpp
+++ src/Operators/dgsem_device_cache.hpp
@@ -1,12 +1,15 @@
 #pragma once
 
 namespace Prandtl
-{ 
+{
   struct DGSEMDeviceCache {
+    int p = 0;
     int dim = 0;
     int num_elements = 0;
     int ndof_scalar_el = 0;
     int num_equations = 0;
+    int num_face_points = 0;
+    int num_interior_faces = 0;
     int Np_x = 0;
     int Np_y = 0;
     int Np_z = 0;
@@ -20,7 +23,19 @@ namespace Prandtl
     const real_t *Dhat_d = nullptr;
     const real_t *Dhat2_d = nullptr;
     real_t *elWaveSpeed_d = nullptr;
+    real_t *ifWaveSpeed_d = nullptr;
+    real_t *bndWaveSpeed_d = nullptr;
     Prandtl::IdealGasModel gas;
     Prandtl::ChandrashekarFlux::InviscidFlux iflux;
+
+
+    MFEM_HOST_DEVICE inline int iface_idx(int side, int fp, int eq) const
+    {
+      return ((side*num_equations + eq)*num_face_points + fp);
+    }
+    MFEM_HOST_DEVICE inline int iface_size() const
+    {
+      return 2*num_equations*num_face_points;
+    }
   };
 }
