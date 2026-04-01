b8a3e24de7eb7809058ef684f0858c137354b505
Mike Campbell <mtcampbe@illinois.edu>
2026-04-01 13:44:15 -0500

    Size alpha in blending-specific cache prep

--- src/Operators/dgsem_cache_utilities.hpp
+++ src/Operators/dgsem_cache_utilities.hpp
@@ -570,6 +570,9 @@ namespace Prandtl {
     const int Np_y = cache->Np_y;
     const int Np_z = cache->Np_z;
     const real_t *D = cache->D.HostRead();
+ 
+    cache->alpha.SetSize(ne);
+    cache->alpha = 0.0;
 
     mfem::DenseTensor SubcellMetricXi, SubcellMetricEta, SubcellMetricZeta;
     SubcellMetricXi.SetSize(dim, Np_z * Np_y * (Np_x + 1), ne);
