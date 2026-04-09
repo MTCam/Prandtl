515ac72d5e83434c52bcbc3979bf3960484491f2
Mike Campbell <mtcampbe@illinois.edu>
2026-04-09 14:15:45 -0500

    Clean up 2DTGV, correct error in init

--- TestCases/NavierStokes/2D/TaylorGreenVortex/TaylorGreenVortex2D.hpp
+++ TestCases/NavierStokes/2D/TaylorGreenVortex/TaylorGreenVortex2D.hpp
@@ -11,13 +11,14 @@ std::function<void(const Vector&, Vector&)> TaylorGreenVortex2DIC(real_t gamma, 
     return [gamma, Ma](const Vector &x, Vector &y)
     {
         MFEM_ASSERT(x.Size() == 2, "");
+        MFEM_ASSERT(y.Size() == 4, "");
 
         real_t den, velX, velY, energy, p, p0 = 1.0 / (gamma * Ma * Ma);
 
         den = 1.0;
         velX = std::sin(x(0)) * std::cos(x(1));
         velY = -std::cos(x(0)) * std::sin(x(1));
-        p = p0 + 0.25 * (std::cos(2.0 * x(0)) + std::cos(2.0 * x(1))) * std::cos(2.0 * x(2) + 2);
+        p = p0 + 0.25 * (std::cos(2.0 * x(0)) + std::cos(2.0 * x(1)));
 
         energy = p / (gamma - 1.0) + 0.5 * den * (velX * velX + velY * velY);
   
