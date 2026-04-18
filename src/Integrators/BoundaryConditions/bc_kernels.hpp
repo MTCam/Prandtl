e0d5fc7b6ce1e295f17f49e763ae05dcbf27f4d6
Mike Campbell <mtcampbe@illinois.edu>
2026-04-18 08:51:17 -0500

    Add kernels for viscous boundary assembly

--- src/Integrators/BoundaryConditions/bc_kernels.hpp
+++ src/Integrators/BoundaryConditions/bc_kernels.hpp
@@ -2,6 +2,7 @@
 
 #include "bc_cache_utilities.hpp"
 #include "Flow.hpp"
+#include "NavierStokesFlux.hpp"
 
 namespace Prandtl {
 
@@ -92,6 +93,62 @@ namespace Prandtl {
       return std::abs(v) + c;
     }
 
+    template<typename GasModelT>
+    MFEM_HOST_DEVICE
+    real_t NoSlipAdiabWallFluxKernel(const GasModelT &gasModel, const real_t *state1,
+                                     const real_t *gradPrim_x, const real_t *gradPrim_y,
+                                     const real_t *gradPrim_z,
+                                     const real_t *nor, const real_t vWall[Prandtl::MAXDIM],
+                                     const real_t qWall, real_t *fluxN)
+    {
+      
+      real_t unit_nor[Prandtl::MAXDIM];
+      real_t state2[Prandtl::MAXEQ];
+      real_t visc_flux[Prandtl::MAXEQ][Prandtl::MAXDIM];
+      const int dim = gasModel.L.dim;
+      const int neq = gasModel.L.nequations();
+      real_t normag = 0.0;
+      for(int idim = 0;idim < dim;idim++){
+        unit_nor[idim] = nor[idim];
+        normag += nor[idim]*nor[idim];
+      }
+      normag = Prandtl::Kernels::rsqrt(normag);
+      
+      for(int ieq = 0;ieq < neq;ieq++){
+        state2[ieq] = state1[ieq];
+        fluxN[ieq] = 0.0;
+      }
+      Prandtl::Kernels::Normalize(dim, unit_nor);
+      Prandtl::PointStateViewRW S{state2};
+      Prandtl::Flow::RotateState(gasModel.L, unit_nor, S);
+      const real_t p_star = Prandtl::Flow::slipwall_pstar(S, gasModel);
+      const real_t v = gasModel.velocity(S, 0); // the "x" component is v*n
+      const real_t c = gasModel.sound_speed(S);
+      const int mom_eq = gasModel.L.eq_mom0;
+      for(int idim = 0;idim < dim;idim++)
+        fluxN[mom_eq+idim] = p_star * nor[idim];
+      // Inviscid part is done, now for the viscous part
+      real_t qn = qWall * normag;
+      NavierStokesFlux::ComputeViscousFluxKernel(gasModel, dim, state1, gradPrim_x, gradPrim_y,
+                                                 gradPrim_z, visc_flux);
+      real_t vflux_n[Prandtl::MAXEQ];
+      for(int j = 0;j < neq;j++){
+        vflux_n[j] = 0.0;
+        for(int idim = 0;idim < dim;idim++){
+          vflux_n[j] += nor[idim]*visc_flux[j][idim];
+        }
+      }
+      const int ener_eq = gasModel.L.eq_energy;
+      vflux_n[ener_eq] = qn;
+      for(int idim = 0;idim < dim;idim++){
+        vflux_n[ener_eq] += vWall[idim]*vflux_n[mom_eq+idim];
+      }
+      for(int j = 0; j < neq;j++){
+        fluxN[j] -= vflux_n[j];
+      }
+      return std::abs(v) + c;
+    }
+
     template <typename DeviceCacheT>
     MFEM_HOST_DEVICE
     real_t ApplyBoundaryConditionInviscid(const DeviceCacheT &dc,
@@ -140,6 +197,44 @@ namespace Prandtl {
               S2.set_momentum(gas.L, idim, mm);
             }
             return dc.iflux.ComputeFaceFlux(gas, state1, bc_state, nor, fluxN);
+          }
+        default:
+          {
+            const int neq = dc.num_equations;
+            for (int eq = 0; eq < neq; ++eq) { fluxN[eq] = 0.0; }
+            return 0.0;
+          }
+        }
+      return 0.0;
+    }
+
+    template <typename DeviceCacheT>
+    MFEM_HOST_DEVICE
+    real_t ApplyViscousBoundaryCondition(const DeviceCacheT &dc,
+                                         const Prandtl::BCDescriptor &bc,
+                                         const real_t *state1,
+                                         const real_t *gradPrim_x,
+                                         const real_t *gradPrim_y,
+                                         const real_t *gradPrim_z,
+                                         const real_t *nor,
+                                         real_t *fluxN)
+    {
+      const auto &gas = dc.gas;
+      const int dim = dc.dim;
+      const real_t *scalar_data = dc.bc_scalar_d;
+      const real_t *vector_data = dc.bc_vector_d;
+      switch (static_cast<Prandtl::BCType>(bc.type))
+        {
+        case Prandtl::BCType::NoSlipAdiab:
+          {
+            const real_t *bc_vec_data = vector_data + bc.data_index;
+            real_t vWall[Prandtl::MAXDIM];
+            for(int idim=0;idim < dim;idim++){
+              vWall[idim] = bc_vec_data[idim];
+            }
+            const real_t qWall = bc_vec_data[dim];
+            return NoSlipAdiabWallFluxKernel(gas, state1, gradPrim_x, gradPrim_y,
+                                             gradPrim_z, nor, vWall, qWall, fluxN);
           }
         default:
           {
