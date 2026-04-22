154eb82aa02393da2d590b7cae3842c962c4d289
Michael T. Campbell <mtcampbe@illinois.edu>
2026-04-22 09:16:20 -0700

    En/disable timers at runtime

--- src/Device/timer.hpp
+++ src/Device/timer.hpp
@@ -6,19 +6,23 @@
 class ScopedTimer
 {
 public:
-    explicit ScopedTimer(const char* name)
-        : name_(name),
-          start_(clock::now()) {}
+#ifdef ENABLE_TIMERS
+  explicit ScopedTimer(const char *name)
+    : name_(name),
+      start_(clock::now()) {}
 
-    ~ScopedTimer()
-    {
-        auto end = clock::now();
-        double ms = std::chrono::duration<double, std::milli>(end - start_).count();
-        std::printf("[TIMER] %s : %.6f ms\n", name_, ms);
-    }
-
+  ~ScopedTimer()
+  {
+    auto end = clock::now();
+    double ms = std::chrono::duration<double, std::milli>(end - start_).count();
+    std::printf("[TIMER] %s : %.6f ms\n", name_, ms);
+  }
+#else
+  explicit ScopedTimer(const std::string &name){};
+  ~ScopedTimer(){};
+#endif
 private:
-    using clock = std::chrono::steady_clock;
-    const char* name_;
-    clock::time_point start_;
+  using clock = std::chrono::steady_clock;
+  const char *name_;
+  clock::time_point start_;
 };
